2de76d1c2c65cd4ee95cd0ea9863b2e13de87249
vikassharma96 <teckudos@gmail.com>
2021-08-07 00:49:17 +0530

    Initial Commit

---
+++ BleBatteryStatus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BleBatteryStatus
+//
+//  Created by Personal on 07/08/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BleBatteryStatus/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BleBatteryStatus
+//
+//  Created by Personal on 07/08/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
